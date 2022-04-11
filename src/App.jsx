d1944c157fbc05be33bfa8eb9779555fcae1b991
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-04-11 23:47:41 +0200

    added conditionally rendered Intro and Quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import IntroPage from "./Components/IntroPage";
+import Quiz from "./Components/Quiz";
 
 function App() {
-	return <IntroPage />;
+	const [isStarted, setIsStarted] = React.useState(false);
+
+	function startQuiz() {
+		console.log("start");
+		setIsStarted((prevIsStarted) => !prevIsStarted);
+	}
+
+	const renderQuiz = !isStarted ? <IntroPage start={() => startQuiz()} /> : <Quiz />;
+
+	return <div>{renderQuiz}</div>;
 }
 
 export default App;
--- src/Components/IntroPage.jsx
+++ src/Components/IntroPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function IntroPage() {
+export default function IntroPage(props) {
 	return (
 		<div className="bg-indigo-200 h-screen flex flex-col items-center justify-center text-center">
 			<h1 className="text-5xl font-bold">Quiz</h1>
 			<h3 className="py-8 text-2xl">Click on button below and try yourself in some questions</h3>
 			<div>
-				<button className="bg-indigo-700 hover:bg-indigo-600 active:bg-indigo-800 px-12 py-4 rounded-xl text-white text-2xl shadow-xl ">
+				<button
+					onClick={props.start}
+					className="bg-indigo-700 hover:bg-indigo-600 active:bg-indigo-800 px-12 py-4 rounded-xl text-white text-2xl shadow-xl "
+				>
 					Start quiz
 				</button>
 			</div>
