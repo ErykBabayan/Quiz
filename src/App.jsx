f3818a779a3d62a4a48a825ab8e3805d96a42bc9
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-04-12 21:30:46 +0200

    Pulling data from quiz api,and pushing quiz object to useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,36 @@ import Quiz from "./Components/Quiz";
 
 function App() {
 	const [isStarted, setIsStarted] = React.useState(false);
+	const [quiz, setQuiz] = React.useState([]);
 
 	function startQuiz() {
 		console.log("start");
 		setIsStarted((prevIsStarted) => !prevIsStarted);
 	}
 
+	React.useEffect(() => {
+		fetch("https://opentdb.com/api.php?amount=5")
+			.then((res) => res.json())
+			.then((data) => {
+				const quizArray = [];
+
+				data.results.map((info, index) => {
+					const allAnswers = info.incorrect_answers.concat(info.correct_answer);
+					const quizElement = {
+						id: index,
+						question: info.question,
+						allAnswers: allAnswers,
+						userAnswer: "",
+					};
+					quizArray.push(quizElement);
+				});
+				setQuiz(quizArray);
+			});
+	}, []);
+
 	const renderQuiz = !isStarted ? <IntroPage start={() => startQuiz()} /> : <Quiz />;
+
+	console.log(quiz);
 
 	return <div>{renderQuiz}</div>;
 }
