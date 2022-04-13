603a7c808e9d0dd1d93456649a58462d5dd3c0b4
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-04-13 21:17:31 +0200

    added basic functionality for rendering quiz questions

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function Quiz() {
-	return <h2>Quiz</h2>;
+export default function Quiz(props) {
+	//console.log(props);
+
+	const quizElement = props.quiz.map((item, index) => {
+		console.log(item);
+
+		return (
+			<form key={index}>
+				<legend>{item.question}</legend>
+				{item.allAnswers.map((answers, index) => {
+					<input type="radio" />;
+				})}
+			</form>
+		);
+	});
+
+	return (
+		<div className=" flex justify-center text-blue-900">
+			<div className="flex flex-wrap justify-center max-w-4xl border border-red-600 border-solid">
+				<div>{quizElement}</div>
+			</div>
+		</div>
+	);
 }
