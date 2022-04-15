e6df0f106b08ebb5196dd0f28a93a0bb2e322189
ErykBabayan <74492818+ErykBabayan@users.noreply.github.com>
2022-04-15 13:03:40 +0200

    exported inputs to separate component and rendering it on screen with more styling

---
+++ src/Components/Answer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Answer(props) {
+	return (
+		<label className=" mx-4">
+			<input type="checkbox" />
+			Answer
+		</label>
+	);
+}
--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Answer from "./Answer";
 
 export default function Quiz(props) {
 	//console.log(props);
@@ -9,16 +10,18 @@ export default function Quiz(props) {
 		return (
 			<form key={index}>
 				<legend>{item.question}</legend>
-				{item.allAnswers.map((answers, index) => {
-					<input type="radio" />;
-				})}
+				<fieldset>
+					{item.allAnswers.map((answers, index) => {
+						return <Answer />;
+					})}
+				</fieldset>
 			</form>
 		);
 	});
 
 	return (
-		<div className=" flex justify-center text-blue-900">
-			<div className="flex flex-wrap justify-center max-w-4xl border border-red-600 border-solid">
+		<div className=" flex justify-center text-blue-900 bg-blue-900 h-screen">
+			<div className="flex flex-wrap justify-center max-w-4xl border border-red-600 border-solid bg-yellow-50 py-12 px-12">
 				<div>{quizElement}</div>
 			</div>
 		</div>
